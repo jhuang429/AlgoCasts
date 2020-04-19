fd54d8dac38dbd1512449d68af90365d20c90db6
jhuang429 <jhuang9240@gmail.com>
2020-04-19 13:12:59 -0400

    Update index.js

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let result = []
+
+    while (array.length > 0) {
+        if (array.length <= size) {
+          result.push(array.splice(0,size))
+        }
+        else{
+          result.push(array.splice(0,size))
+        }
+    }
+    return result 
+}
+
 
 module.exports = chunk;
